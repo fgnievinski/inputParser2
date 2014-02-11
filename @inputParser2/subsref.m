238e3fca75b3b27949ac615f78d2c4c946f13800
fgnievinski <fgnievinski@gmail.com>
2014-02-11 13:21:03 -0200

    minor cleanup of subsref

--- @inputParser2/subsref.m
+++ @inputParser2/subsref.m
@@ -5,17 +5,15 @@ function out = subsref (inPar2, idx)
     % make sure this matlab-incompatible syntax is not available:
     error('Invalid syntax; call dispose(...) instead');
   case {'parse','createCopy'}
+    argin = {};
     if (numel(idx) > 1) && strcmp(idx(2).type, '()')
       argin = idx(2).subs;
-    else
-      argin = {};
     end
     out = feval(idx(1).subs, inPar2, argin{:});
   case {'Results', 'Parameters', 'Unmatched', 'UsingDefaults'}
     inPar = inputParserStore ('pull', inPar2.counter);
     inPar = struct(inPar);
     out = subsref (inPar, idx);
-    % WRONG! unimplemented
   case {'addOptional', 'addParamValue', 'addRequired'}
     inPar = inputParserStore ('pull', inPar2.counter);
     inPar = subsref (inPar, idx);
